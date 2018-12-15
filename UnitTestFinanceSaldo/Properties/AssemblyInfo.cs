9db5da6cadfffe50b4bfa9d6268e1a6ddf7fd405
merd1978 <nss1978@gmail.com>
2018-12-15 18:53:00 +0000

    added basic unit test

---
+++ UnitTestFinanceSaldo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFinanceSaldo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFinanceSaldo")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ecc4c76-e65a-4314-8f91-ed14ab881766")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestFinanceSaldo/UnitTestIvoiceViewModel.cs
@@ -0,0 +1,28 @@
+﻿using FinanceSaldo.Design;
+using FinanceSaldo.Model;
+using FinanceSaldo.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFinanceSaldo
+{
+    [TestClass]
+    public class UnitTestIvoiceViewModel
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arange
+            var testDataService = new DesignDataService();
+            var company = new Company() {Name = "test"};
+            var invoice = new Invoice();
+            var invoiceViewModel = new InvoiceViewModel(testDataService, company);
+
+            //Act
+            invoice.Debit = 120;
+            company.Invoice.Add(invoice);
+
+            //Assert
+            Assert.AreEqual(120, company.TotalSaldo);
+        }
+    }
+}
